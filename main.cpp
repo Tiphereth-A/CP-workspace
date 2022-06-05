c5222ff6e662f41b0bfb392f046b19b94a4ab308
Tifa <tiphereth-a@qq.com>
2022-06-06 00:59:03 +0800

    fix: upd default

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// #define DISABLE_PRAGMA_
 #ifndef DISABLE_PRAGMA_
 #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4.1,sse4.2,avx,avx2,popcnt,tune=native")
