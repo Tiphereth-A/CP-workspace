6fef048e78298ed249d54dd7efd214ea4812ddb5
Tifa <tiphereth-a@qq.com>
2022-06-06 17:34:56 +0800

    fix: remove duplicate inline

--- tools/lib-cpp/template/xor_basis.hpp
+++ tools/lib-cpp/template/xor_basis.hpp
@@ -33,21 +33,21 @@ class Xor_base {
         for (auto &&i : std::move(_list)) this->insert(i);
     }
 
-    inline constexpr void clear() noexcept { memset(this->base, 0, sizeof(this->base)); }
+    constexpr void clear() noexcept { memset(this->base, 0, sizeof(this->base)); }
 
-    inline constexpr size_type get_len() const noexcept { return N; }
+    constexpr size_type get_len() const noexcept { return N; }
 
-    inline constexpr data_type &data(size_type index) noexcept { return this->base[index]; }
-    inline constexpr data_type &data(size_type index) const noexcept { return const_cast<self * const>(this)->base[index]; }
+    constexpr data_type &data(size_type index) noexcept { return this->base[index]; }
+    constexpr data_type &data(size_type index) const noexcept { return const_cast<self * const>(this)->base[index]; }
 
-    inline constexpr data_type operator[](size_type index) noexcept { return this->data(index); }
-    inline constexpr data_type operator[](size_type index) const noexcept { return const_cast<self * const>(this)->data(index); }
+    constexpr data_type operator[](size_type index) noexcept { return this->data(index); }
+    constexpr data_type operator[](size_type index) const noexcept { return const_cast<self * const>(this)->data(index); }
 
-    inline constexpr iterator begin() noexcept { return this->base; }
-    inline constexpr const_iterator begin() const noexcept { return const_cast<data_type * const>(this->base); }
+    constexpr iterator begin() noexcept { return this->base; }
+    constexpr const_iterator begin() const noexcept { return const_cast<data_type * const>(this->base); }
 
-    inline constexpr iterator end() noexcept { return this->begin() + this->get_len(); }
-    inline constexpr const_iterator end() const noexcept { return this->begin() + this->get_len(); }
+    constexpr iterator end() noexcept { return this->begin() + this->get_len(); }
+    constexpr const_iterator end() const noexcept { return this->begin() + this->get_len(); }
 
     inline reverse_iterator rbegin() noexcept { return reverse_iterator(this->end()); }
     inline const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator(this->end()); }
@@ -55,7 +55,7 @@ class Xor_base {
     inline reverse_iterator rend() noexcept { return reverse_iterator(this->begin()); }
     inline const_reverse_iterator rend() const noexcept { return const_reverse_iterator(this->begin()); }
 
-    inline constexpr self &insert(data_type x) noexcept {
+    constexpr self &insert(data_type x) noexcept {
         this->status = false;
         for (size_type i = this->get_len() - 1; ~i; --i) {
             if (!(x & (1ull << i))) continue;
@@ -74,15 +74,15 @@ class Xor_base {
         return *this;
     }
 
-    inline constexpr bool get_status() const noexcept { return this->status; }
+    constexpr bool get_status() const noexcept { return this->status; }
 
-    inline constexpr data_type max_span() const noexcept {
+    constexpr data_type max_span() const noexcept {
         data_type ret(0);
         for (const auto &i : this->base) ret ^= i;
         return ret;
     }
 
-    inline constexpr size_type rank() const noexcept {
+    constexpr size_type rank() const noexcept {
         size_type ret(0);
         for (size_type i = 0; i < this->get_len(); ++i) ret += !!(this->base[i] & (1ull << i));
         return ret;
