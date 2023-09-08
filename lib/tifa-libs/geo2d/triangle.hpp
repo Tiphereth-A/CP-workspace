c7bb9b26371d86e3ea6977ee97a3cdc6ed3edc09
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-09-08 12:52:31 +0800

    chore: migrate to CP-lib

--- lib/tifa-libs/geo2d/triangle.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef TIFALIBS_GEO2D_TRIANGLE_HPP
-#define TIFALIBS_GEO2D_TRIANGLE_HPP
-
-#include <iostream>
-
-#include "tifa-libs/geo2d/util.hpp"
-#include "tifa-libs/geo2d/triangle_class.hpp"
-
-#endif
