deb22dde22b1656ef5795d2318939815ea6ffa9f
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-12-26 23:13:18 +0800

    fix: tiny change

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #define DISABLE_PRAGMA_
 #ifndef DISABLE_PRAGMA_
 #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4.1,sse4.2,avx,avx2,popcnt,tune=native")
+#pragma GCC target("avx,avx2,popcnt,tune=native")
 #endif
 
 #include <bits/stdc++.h>
